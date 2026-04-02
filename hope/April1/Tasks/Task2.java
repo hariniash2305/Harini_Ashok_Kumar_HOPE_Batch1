aaafb90bfbddce6528443d920c267c9fd9f6a597
Harini Ashok <hariniash2305@gmail.com>
2026-04-02 07:26:34 +0530

    Add files via upload

---
+++ hope/April1/Tasks/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        Float f1=127f;
+        Float f2=127f;
+        System.out.println(f1==f2);//false
+        Integer x=200;
+        Integer y=200;
+        System.out.println(x==y);//false
+        Integer a=127;
+        Integer b=127;
+        System.out.println(a==b);//true
+    }
+}
