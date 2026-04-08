9e53719be51e2a3d5d4e60578c215181501aa864
Harini Ashok <hariniash2305@gmail.com>
2026-04-08 07:27:41 +0530

    Add files via upload

---
+++ hope/April7/404.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n==1) return true;
+        Set<Integer> set=new HashSet<>();
+        while(n!=1){
+            if(set.contains(n)) return false;
+            set.add(n);
+            int sum=0;
+            while(n>0){
+                int digit=n%10;
+                sum+=digit*digit;
+                n/=10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+}
