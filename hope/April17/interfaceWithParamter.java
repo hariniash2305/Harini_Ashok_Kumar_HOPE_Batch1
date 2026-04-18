b94035e0a7f244e8b577f1074c6dd2e3f127ffb6
Harini Ashok <hariniash2305@gmail.com>
2026-04-18 09:19:00 +0530

    Add files via upload

---
+++ hope/April17/interfaceWithParamter.java
@@ -0,0 +1,11 @@
+
+
+public class interfaceWithParameter {
+    public static void main(String[] args) {
+        Test obj=(a,b)-> System.out.println("Hello lambda interface  "+(a+b));
+        obj.show(10,20);
+    }
+}
+interface Test{
+    public void show(int a,int b);
+}
